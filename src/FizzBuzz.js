656c6c2fc7b3bcfc580669a797aed3da1b3f18a9
Pohaki <kevin.lachaise@gmail.com>
2015-11-24 17:09:34 +0100

    ajout de l'exercie

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,5 +1,18 @@
 function fizzBuzz(i) {
-  
+  var Fizz = "Fizz";
+  var Buzz = "Buzz";
+  if (i % 15 === 0){
+    return Fizz + Buzz;
+  }
+  else if (i % 5 === 0){
+    return Buzz;
+  }
+  else if (i % 3 === 0){
+    return Fizz;
+  }
+  else {
+    return i;
+  }
 }
 
 module.exports = fizzBuzz;
